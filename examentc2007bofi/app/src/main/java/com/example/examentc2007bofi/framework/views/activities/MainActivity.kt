2ac22679e82be0ccbe1f678a1e79adff93e5391a
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2024-11-25 18:04:18 -0600

    Agrega sistemas de colores para identificar regiones contagiadas

--- examentc2007bofi/app/src/main/java/com/example/examentc2007bofi/framework/views/activities/MainActivity.kt
+++ examentc2007bofi/app/src/main/java/com/example/examentc2007bofi/framework/views/activities/MainActivity.kt
@@ -1,53 +1,36 @@
 package com.example.secondrecycleview.framework.views.activities
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.secondrecycleview.CovidDataProvider
 import com.example.secondrecycleview.framework.adapters.CovidDataAdapter
 import com.example.secondrecycleview.databinding.ActivityMainBinding
-import com.example.secondrecycleview.framework.viewmodel.CovidDataViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: CovidDataAdapter
-    private var isSorted = false
-    private val viewModel: CovidDataViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel.getCovidData()
-        initRecyclerView()
-        setupButtons()
+
+        val average = calculateAverage()
+
+        initRecyclerView(average)
     }
 
-    private fun initRecyclerView() {
-        adapter = CovidDataAdapter(CovidDataProvider.covidDataList)
+    private fun calculateAverage(): Double {
+        val totalCases = CovidDataProvider.covidDataList.flatMap { it.cases.values }.sumOf { it.total }
+        val numberOfCases = CovidDataProvider.covidDataList.flatMap { it.cases.values }.size
+        return if (numberOfCases > 0) totalCases.toDouble() / numberOfCases else 0.0
+    }
+
+    private fun initRecyclerView(average: Double) {
+        adapter = CovidDataAdapter(CovidDataProvider.covidDataList, average)
         binding.recycleCovidData.layoutManager = GridLayoutManager(this, 2) // Grid con 2 columnas
         binding.recycleCovidData.adapter = adapter
-    }
-
-    private fun setupButtons() {
-
-        binding.btnSortAlphabetically.setOnClickListener {
-            toggleSort()
-        }
-    }
-
-    private fun toggleSort() {
-        if (isSorted) {
-            adapter = CovidDataAdapter(CovidDataProvider.covidDataList)
-            binding.recycleCovidData.adapter = adapter
-            isSorted = false
-        } else {
-            val sortedList = CovidDataProvider.covidDataList.sortedBy { it.region }
-            adapter = CovidDataAdapter(sortedList)
-            binding.recycleCovidData.adapter = adapter
-            isSorted = true
-        }
     }
 }
